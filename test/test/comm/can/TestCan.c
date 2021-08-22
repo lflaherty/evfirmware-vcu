1174ccb7c5e210fe815d4c69c1271a8acc609a64
Liam Flaherty <liamjfla@gmail.com>
2021-08-23 00:37:40 +1000

    added test framework

---
+++ test/test/comm/can/TestCan.c
@@ -0,0 +1,18 @@
+#include "unity.h"
+#include "unity_fixture.h"
+//#include "comm/can/can.h"
+
+TEST_GROUP(COMM_CAN);
+
+TEST_SETUP(COMM_CAN)
+{
+}
+
+TEST_TEAR_DOWN(COMM_CAN)
+{
+}
+
+TEST(COMM_CAN, TestName)
+{
+}
+
---
+++ test/test/comm/can/TestCanRunner.c
@@ -0,0 +1,7 @@
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(COMM_CAN)
+{
+	RUN_TEST_CASE(COMM_CAN, TestName);
+}
