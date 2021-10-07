7b851adf2c76ce80a021414fb569155567c79bdb
Liam Flaherty <liamjfla@gmail.com>
2021-10-07 22:29:54 +1000

    remove static data storage for external devices

--- time/externalWatchdog/externalWatchdog.h
+++ time/externalWatchdog/externalWatchdog.h
@@ -12,6 +12,12 @@
 
 #include "stm32f7xx_hal.h"
 
+typedef struct
+{
+  GPIO_TypeDef* gpioBank; // GPIO Port used for WDG
+  uint16_t gpioPin;       // GPIO Pin used for WDG
+} ExternalWatchdog_T;
+
 typedef enum
 {
   EXTWATCHDOG_STATUS_OK     = 0x00U
@@ -23,12 +29,12 @@ typedef enum
  * @param GPIOx GPIO bank for ext WDG trigger
  * @param GPIO_Pin GPIO pin for ext WDG trigger
  */
-ExternalWatchdog_Status_T ExternalWatchdog_Init(Logging_T* logger, GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+ExternalWatchdog_Status_T ExternalWatchdog_Init(Logging_T* logger, ExternalWatchdog_T* wdg);
 
 /**
  * @brief Trigger the external watchdog timer to avoid reset.
  * Call this periodically.
  */
-ExternalWatchdog_Status_T ExternalWatchdog_Trigger(void);
+ExternalWatchdog_Status_T ExternalWatchdog_Trigger(ExternalWatchdog_T* wdg);
 
 #endif /* TIME_EXTERNALWATCHDOG_EXTERNALWATCHDOG_H_ */
