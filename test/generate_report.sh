d99c6699c57b18c4e60b27bb22721499e62a36bc
Liam Flaherty <liamjfla@gmail.com>
2021-10-15 22:38:46 +1000

    update report generation

--- test/generate_report.sh
+++ test/generate_report.sh
@@ -3,16 +3,18 @@ set -e
 
 echo Running tests
 # Run test to generate coverage files
-find . -maxdepth 1 -regex "\.\/Test[a-zA-z]*" -exec {} \;
+for file in `find . -maxdepth 1 -regex "\.\/Test[a-zA-z]*"`
+do
+	echo "Running $file"
+	./$file
+done
 
-TEST_LIB_FILES=\
-	coverage/mock* \
-	coverage/Mock* \
-	coverage/unity* \
-	coverage/Test*
+# Remove files not to include in the coverage report
+rm -f coverage/mock*
+rm -f coverage/Mock*
+rm -f coverage/unity*
+rm -f coverage/Test*
 
-rm -f "$TEST_LIB_FILES"
-
-echo Generating report
+# echo Generating report
 lcov -c -d ./coverage -o main_coverage.info
 genhtml main_coverage.info --output-directory coverage_report
