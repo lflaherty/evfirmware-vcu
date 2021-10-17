1eaa1d86c40e2fdf4001f6ff06173ff54cc6d060
Liam Flaherty <liamjfla@gmail.com>
2021-10-17 14:19:26 +1000

    allow test system to be called from larger test framework

--- test/test_common.mk
+++ test/test_common.mk
@@ -34,29 +34,54 @@ CFLAGS += -fprofile-dir=./coverage
 # Linker flags
 LFLAGS=-lm
 
+# The directory to build in, referenced from location of test_common.mk
+# $(TEST_ROOT) references . as being the root of the evfirmware-lib/test directory
+# Just have this as $(TEST_ROOT) if building from the evfirmware-lib repo, but change this to
+# $(TEST_ROOT)/other_directory if building into a larger set of tests
+# BUILD_DIR=$(TEST_ROOT)/../../test
+# You can set BUILD_DIR_CD as a make parameter to change this
+ifeq ($(BUILD_DIR_CD),)
+BUILD_DIR=$(TEST_ROOT)
+else
+BUILD_DIR=$(TEST_ROOT)/$(BUILD_DIR_CD)
+endif
+
+# $(LIB_TEST_DIR) is the relactive directory to locate evfirmware-lib/test from current directory
+# just have this as . if building from evfirmare-lib repo, but if building from another set of tests,
+# set this to nagivate to evfirmare-lib/test
+ifeq ($(LIB_TEST_DIR),)
+LIB_TEST_DIR=.
+endif
+
 UNITY_ROOT=Unity
 
 COMMON_MOCKS=\
 	mock/FreeRTOS/mockFreeRTOS.c \
 	mock/FreeRTOS/mockQueue.c \
 	mock/FreeRTOS/mockTask.c \
+	mock/FreeRTOS/mockSemphr.c \
 	mock/stm32_hal/MockStm32f7xx_hal.c \
 	mock/stm32_hal/MockStm32f7xx_hal_can.c \
-	mock/stm32_hal/MockStm32f7xx_hal_adc.c \
+	mock/stm32_hal/MockStm32f7xx_hal_adc.c
 
-SRC_FILES=\
+SRC_FILES=$(addprefix $(LIB_TEST_DIR)/,\
 	$(UNITY_ROOT)/src/unity.c \
 	$(UNITY_ROOT)/extras/fixture/src/unity_fixture.c \
 	$(COMMON_MOCKS) \
 	$(MOCKS) \
 	$(TESTED_SRC) \
-	test/$(TEST_SRC)
+	test/$(TEST_SRC) \
+	)
 
-INC_DIRS=-I../src -Itest -Imock -I$(UNITY_ROOT)/src -I$(UNITY_ROOT)/extras/fixture/src
+INC_DIRS=-I$(LIB_TEST_DIR)/../src
+INC_DIRS+=-I$(LIB_TEST_DIR)/test
+INC_DIRS+=-I$(LIB_TEST_DIR)/mock
+INC_DIRS+=-I$(LIB_TEST_DIR)/$(UNITY_ROOT)/src
+INC_DIRS+=-I$(LIB_TEST_DIR)/$(UNITY_ROOT)/extras/fixture/src
 SYMBOLS=-DUNITY_FIXTURE_NO_EXTRAS
 
 default:
 	@echo Build...
-	mkdir -p $(TEST_ROOT)/coverage
-	cd $(TEST_ROOT) && $(GCC) $(CFLAGS) $(INC_DIRS) $(SYMBOLS) $(SRC_FILES) $(LFLAGS) -o $(TARGET)
-	mv $(patsubst %.c,$(TEST_ROOT)/%.gcno,$(notdir $(TESTED_SRC))) $(TEST_ROOT)/coverage
+	mkdir -p $(BUILD_DIR)/coverage
+	cd $(BUILD_DIR) && pwd && $(GCC) $(CFLAGS) $(INC_DIRS) $(SYMBOLS) $(SRC_FILES) $(LFLAGS) -o $(TARGET)
+	mv $(patsubst %.c,$(BUILD_DIR)/%.gcno,$(notdir $(TESTED_SRC))) $(BUILD_DIR)/coverage
