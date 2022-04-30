992b2195327e8f9b956702d68e8c88ad453cdab7
Liam Flaherty <liamjfla@gmail.com>
2022-05-01 03:14:43 +1000

    pass directories to add to report via script args

--- test/generate_report.sh
+++ test/generate_report.sh
@@ -2,11 +2,23 @@
 set -e
 
 if [ -z $1 ]; then
-    echo "Usage: generate_report.sh path_to_path"
+    echo "Usage: generate_report.sh path_to_src1 ..."
     exit 1
 fi
-src_path=$1
-full_src_path="$(cd ${src_path} && pwd)"
+
+echo "Reading paths"
+SRC_LIST=()
+for src_path in "$@"
+do
+    if [ -d "$src_path" ]; then
+        full_src_path="$(cd ${src_path} && pwd)"
+        SRC_LIST+=("${full_src_path}*")
+        echo "$src_path -> $full_src_path"
+    else
+        echo "$src_path is not a directory"
+        exit 1
+    fi
+done
 
 # Reset counters
 lcov --directory . --zerocounters -q
@@ -18,8 +30,8 @@ echo
 echo Generating report
 # Create report for all files
 lcov --directory . -c -o main_coverage.info
-# Only include ../src directory (i.e. exlucde test and mock helper code)
-lcov --extract main_coverage.info "${full_src_path}*" -o main_coverage.info
+# Only include directories requested
+echo "${SRC_LIST[*]}" | xargs lcov -o main_coverage.info --extract main_coverage.info
 
 # Create report webpage
 rm -rf ../coverage_report
