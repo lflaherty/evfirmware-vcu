e6cc0addc1b8266a19393a1670304139780950e6
Liam Flaherty <liamjfla@gmail.com>
2022-05-01 00:18:11 +1000

    allow specifying src path as argument

--- test/generate_report.sh
+++ test/generate_report.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ -z $1 ]; then
+    echo "Usage: generate_report.sh path_to_path"
+    exit 1
+fi
+src_path=$1
+full_src_path="$(cd ${src_path} && pwd)"
+
 # Reset counters
 lcov --directory . --zerocounters -q
 
@@ -12,8 +19,7 @@ echo Generating report
 # Create report for all files
 lcov --directory . -c -o main_coverage.info
 # Only include ../src directory (i.e. exlucde test and mock helper code)
-src_path="$(cd ../src && pwd)"
-lcov --extract main_coverage.info "${src_path}*" -o main_coverage.info
+lcov --extract main_coverage.info "${full_src_path}*" -o main_coverage.info
 
 # Create report webpage
 rm -rf ../coverage_report
