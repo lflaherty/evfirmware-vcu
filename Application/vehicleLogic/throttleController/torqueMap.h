2193d6167be8975ccde7d859e5f00d94c8e71fcb
Liam Flaherty <liamjfla@gmail.com>
2022-06-23 00:23:58 +0930

    remove un-needed code

--- Application/vehicleLogic/throttleController/torqueMap.h
+++ Application/vehicleLogic/throttleController/torqueMap.h
@@ -16,26 +16,17 @@ typedef struct
 {
   float accel[TORQUEMAP_LENGTH]; // accelerator pedal %
   float torque[TORQUEMAP_LENGTH]; // motor torque
-  float continuousTorque; // Nm to cap at for continuous torque mode
-  uint32_t peakTorqueMs; // ms allowed at peak torque
-  uint32_t resetTimeMs; // ms at continuous Tq to allow peak again
 } TorqueMap_T;
 
 static TorqueMap_T TorqueMap_Default =
 {
   .accel =  {0.0f, 0.1f,   0.5f,   0.7f,   1.0f},
-  .torque = {0.0f, 0.0f, 100.0f, 200.0f, 500.0f},
-  .continuousTorque = 213.0f,
-  .peakTorqueMs = 3000U,
-  .resetTimeMs = 3000U
+  .torque = {0.0f, 0.0f, 100.0f, 200.0f, 500.0f}
 };
 static TorqueMap_T TorqueMap_DefaultReverse =
 {
   .accel =  {0.0f, 0.1f,   0.5f,   0.7f,   1.0f},
-  .torque = {0.0f, 0.0f,  20.0f,  75.0f,  75.0f},
-  .continuousTorque = 75.0f,
-  .peakTorqueMs = 3000U,
-  .resetTimeMs = 3000U
+  .torque = {0.0f, 0.0f,  20.0f,  75.0f,  75.0f}
 };
 
 /**
