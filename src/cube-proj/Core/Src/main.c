efbc8f30ed29a2389aab6e015d701a588bfb0e11
Liam Flaherty <liamjfla@gmail.com>
2022-12-31 13:40:01 +1030

    fix hardware timer to expire at 100Hz

--- src/cube-proj/Core/Src/main.c
+++ src/cube-proj/Core/Src/main.c
@@ -707,7 +707,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 9;
+  htim2.Init.Prescaler = 999;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 1079;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
